098e8ebf77cb2da239246a2cc99ea77f0ea54173
newrims <rimsmacbook@rims-mac-book.local>
2025-11-24 11:36:20 +0900

    Add initial iOS Xcode project

---
+++ App/App/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  App
+//
+//  Created by 🍑혜리미 맥북🍑 on 11/24/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ App/AppTests/AppTests.swift
@@ -0,0 +1,17 @@
+//
+//  AppTests.swift
+//  AppTests
+//
+//  Created by 🍑혜리미 맥북🍑 on 11/24/25.
+//
+
+import Testing
+@testable import App
+
+struct AppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
